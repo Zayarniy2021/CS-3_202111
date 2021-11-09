996f16b58246f5c89b99fed51977ea891dc478e1
Иванов Иван <zaazaa@yandex.ru>
2021-11-09 20:47:54 +0300

    Add project files.

---
+++ MailSendTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Diagnostics;
+
+namespace MailSendTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("somemail@gmail.com", "Tom");
+            // кому отправляем
+            MailAddress to = new MailAddress("somemail@yandex.ru");
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            // тема письма
+            m.Subject = "Тест";
+            // текст письма
+            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            // логин и пароль
+            Debug.WriteLine("Message has sent");
+            Console.Read();
+        }
+    }
+}
