a628ea57c1167afed6441ffb7dce98da29b6cf68
Иванов Иван <zaazaa@yandex.ru>
2021-11-12 13:54:10 +0300

    Добавлены TextBoxes, добавлен стиль круглой кнопки, добавлен цвет для TextBox через стиль

--- Utilites/MainWindow.xaml.cs
+++ Utilites/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MailSendTest;
 
 namespace Utilites
 {
@@ -23,6 +24,11 @@ namespace Utilites
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            Library.MailSend();
         }
     }
 }
